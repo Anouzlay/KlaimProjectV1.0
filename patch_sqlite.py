d7a068bec062501c31a130070d28d2527ad67a9a
Theuchiwa <53660048+Anouzlay@users.noreply.github.com>
2025-05-19 19:39:21 +0100

    Update patch_sqlite.py

--- patch_sqlite.py
+++ patch_sqlite.py
@@ -1,16 +1,16 @@
-# # patch_sqlite.py
-# import sys
-# import sqlite3
-# import pysqlite3
-# import os
+# patch_sqlite.py
+import sys
+import sqlite3
+import pysqlite3
+import os
 
-# def patch_sqlite():
-#     print("Current SQLite version:", sqlite3.sqlite_version)
+def patch_sqlite():
+    print("Current SQLite version:", sqlite3.sqlite_version)
     
-#     try:
+    try:
     
-#         sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-#         print("SQLite patched with pysqlite3")
-#         print("New SQLite version:", sqlite3.sqlite_version)
-#     except ImportError:
-#         print("pysqlite3 not found, using system SQLite")
+        sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+        print("SQLite patched with pysqlite3")
+        print("New SQLite version:", sqlite3.sqlite_version)
+    except ImportError:
+        print("pysqlite3 not found, using system SQLite")
